1645755b523a3bfab7c6e9a7d44e3a578c0cb4c3
Manas Bhatt <manasbhatt2002@gmail.com>
2025-11-07 15:36:53 +0530

    coonect to mongo db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
- 
+// require('dotenv').config({path: './env})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js ";
+import mongoose from "mongoose";
+
+dotenv.config({
+    path: './.env',
+})
+
+connectDB()
