327ca4b75b745387140bc45a4101110afb8e735c
Manas Bhatt <manasbhatt2002@gmail.com>
2025-11-10 17:08:30 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,37 @@
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+     Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+
+
+ 
+
+
+
+
+
+export {asyncHandler}
+
+//const asyncHandler=()=>{}
+//const asyncHandler= (func)=>()=>{}
+
+//const asyncHandler= (func)=>(async)=>{}
+ 
+
+    const asyncHandler=(fnc)=>async(req,res,next)=>{
+        try{
+            await fnc(req,res,next)
+            
+        } catch(error){
+            res.status(error.code || 500).json({
+                success: false,
+                message: err.message
+        })
+                
+            } 
+
+        
+    }
